da732087441693f93d2ea4b601a87ddfab8328db
michael <hmichaelsintayehu@gmail.com>
2024-03-20 14:32:27 -0400

    record,delete end point added

--- website/views.py
+++ website/views.py
@@ -6,11 +6,6 @@ from .models import Record
 def home(request):
     records = Record.objects.all()
 
-
-
-
-
-
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
@@ -54,3 +49,21 @@ def register_user(request):
         form = SignUpForm()
         return render(request, 'register.html' , {'form': form})
     return render(request, 'register.html' , {'form': form})
+
+def customer_record(request,pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id = pk)   
+        return render(request, 'record.html' , {'customer_record': customer_record})
+    else:
+        messages.success(request , "You must be Logged in to view that page...")
+        return redirect('home')
+    
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects(id = pk)
+        delete_it.delete()
+        messages.success(request , "Records Deleted Successfully...")
+        return redirect('home')
+    else:
+        messages.success(request , "You must be Logged In to do that...")
+        return redirect('home')
